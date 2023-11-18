bc5b95d0bba9fcf4354c07e692bfcc5eed338921
Vadim Gartman <v.gartman@qiwi.com>
2023-11-18 23:03:58 +0300

    VueCalculator solved

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,31 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      number1: 0,
+      number2: 0,
+      result: 0,
+    };
+  },
+  methods: {
+    calculateNumbers(action) {
+      switch (action) {
+        case 'sum':
+          return this.number1 + this.number2
+        case 'mul':
+          return this.number1 * this.number2
+        case 'sub':
+          return this.number1 - this.number2
+        case 'div':
+          return this.number1 / this.number2
+      }
+    }
+  }
+})
+
+const app = createApp(App);
+const vm = app.mount('#app')
+
+window.vm = vm
