4793a6c264e82419ab438f59464860023e8ae63a
Vadim Gartman <v.gartman@qiwi.com>
2023-11-22 22:53:20 +0300

    MeetupTitle solved

--- 01-basics/04-MeetupTitle/script.js
+++ 01-basics/04-MeetupTitle/script.js
@@ -1,17 +1,44 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 const API_URL = 'https://course-vue.javascript.ru/api';
 
-function fetchMeetupById(meetupId) {
-  return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      return response.json().then((error) => {
-        throw error;
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      meetupId: undefined,
+      meetupTitle: undefined
+    };
+  },
+
+  watch: {
+    "meetupId": {
+      handler(newValue, oldValue) {
+        this.fetchMeetupById(newValue).then((resp) => {
+            this.meetupTitle = resp.title;
+          })
+      }
+    }
+  },
+
+  methods: {
+    fetchMeetupById(meetupId) {
+      return fetch(`${API_URL}/meetups/${meetupId}`).then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((error) => {
+            throw error;
+          });
+        }
       });
     }
-  });
-}
+  },
+})
 
-// Требуется создать Vue приложение
+const app = createApp(App);
+const vm = app.mount('#app')
+
+window.vm = vm
+
+
