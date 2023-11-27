98cfe27ff1388df321d69803769b28f51e28eda7
Vadim Gartman <v.gartman@qiwi.com>
2023-11-27 09:55:12 +0300

    MeetupAgenda solved

--- 02-components/05-MeetupAgenda/components/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/components/MeetupAgendaItem.js
@@ -1,23 +1,41 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
-// import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from '../meetupService.js';
 
 export default defineComponent({
   name: 'MeetupAgendaItem',
 
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true
+    }
+  },
+
+  methods: {
+    getIcon(type) {
+      return `../../src/assets/icons/item-${agendaItemIcons[type]}.svg`
+    },
+    getTitle(item) {
+      return item.title ? item.title : agendaItemDefaultTitles[item.type]
+    }
+  },
+
   template: `
     <div class="agenda-item">
       <div class="agenda-item__col">
-        <img src="/assets/icons/icon-key.svg" class="icon" alt="key" />
+        <img :src="getIcon(agendaItem.type)" class="icon" alt="key" />
       </div>
-      <div class="agenda-item__col">00:00 - 00:00</div>
+      <div class="agenda-item__col">{{ agendaItem.startsAt + ' - ' + agendaItem.endsAt }}</div>
       <div class="agenda-item__col">
-        <h3 class="agenda-item__title">Title</h3>
-        <p class="agenda-item__talk">
-          <span>Talk Speaker</span>
+        <h3 class="agenda-item__title">
+          {{ getTitle(agendaItem) }}
+        </h3>
+        <p v-if="agendaItem.type === 'talk'" class="agenda-item__talk">
+          <span>{{ agendaItem.speaker }}r</span>
           <span class="agenda-item__dot"></span>
-          <span class="agenda-item__lang">EN</span>
+          <span class="agenda-item__lang">{{ agendaItem.language }}</span>
         </p>
-        <p>Description</p>
+        <p>{{ agendaItem.description }}</p>
       </div>
     </div>`,
 });
