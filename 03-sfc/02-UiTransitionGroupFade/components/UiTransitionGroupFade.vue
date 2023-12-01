cd5e2590a5d0d4fc7f018cda428f9e3d9e062c53
Vadim Gartman <v.gartman@qiwi.com>
2023-12-01 18:29:50 +0300

    transition fix

--- 03-sfc/02-UiTransitionGroupFade/components/UiTransitionGroupFade.vue
+++ 03-sfc/02-UiTransitionGroupFade/components/UiTransitionGroupFade.vue
@@ -18,29 +18,32 @@ export default {
 </script>
 
 <style scoped>
+:slotted(*) {
+  .fade-list {
+    position: relative;
+  }
+
+  .fade-list > * {
+    opacity: 1;
+    transition: opacity 0.3s ease-out;
+  }
+
+  .fade-list .fade-list-leave-active {
+    position: absolute !important;
+    left: 0;
+    right: 0;
+  }
+
+  .fade-list .fade-list-enter-from,
+  .fade-list .fade-list-leave-to {
+    opacity: 0;
+  }
+
+  .fade-list .fade-list-move {
+    transition: transform 0.3s;
+  }
+}
 /* _transitions.css */
 
-.fade-list {
-  position: relative;
-}
 
-.fade-list > * {
-  opacity: 1;
-  transition: opacity 0.3s ease-out;
-}
-
-.fade-list .fade-list-leave-active {
-  position: absolute !important;
-  left: 0;
-  right: 0;
-}
-
-.fade-list .fade-list-enter-from,
-.fade-list .fade-list-leave-to {
-  opacity: 0;
-}
-
-.fade-list .fade-list-move {
-  transition: transform 0.3s;
-}
 </style>
