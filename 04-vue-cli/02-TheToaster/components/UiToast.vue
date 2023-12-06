e84ff2e526c2a1ef03c6f5d8ef3e5242a818b204
Vadim Gartman <v.gartman@qiwi.com>
2023-12-06 15:19:59 +0300

    Toaster solved

---
+++ 04-vue-cli/02-TheToaster/components/UiToast.vue
@@ -0,0 +1,69 @@
+<template>
+  <div :class="`toast toast_${type}`">
+    <UiIcon class="toast__icon" :icon="toastIcon" />
+    <span>{{ message }}</span>
+  </div>
+</template>
+
+<script>
+import UiIcon from './UiIcon.vue';
+
+const icons = {
+  "success": "check-circle",
+  "error": "alert-circle",
+}
+export default {
+  name: 'UiToast',
+
+  components: { UiIcon },
+
+  props: {
+    message: {
+      type: String
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  },
+
+  computed: {
+    toastIcon() {
+      return icons[this.type]
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+  .toast {
+    display: flex;
+    flex: 0 0 auto;
+    flex-direction: row;
+    align-items: center;
+    padding: 16px;
+    background: #ffffff;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+    border-radius: 4px;
+    font-size: 18px;
+    line-height: 28px;
+    width: auto;
+  }
+
+  .toast + .toast {
+    margin-top: 20px;
+  }
+
+  .toast__icon {
+    margin-right: 12px;
+  }
+
+  .toast.toast_success {
+    color: var(--green);
+  }
+
+  .toast.toast_error {
+    color: var(--red);
+  }
+</style>
